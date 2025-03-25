3c6f7815d9e4bbb13e910f5b82058fdb14e81755
meshenka <meshee.knight@gmail.com>
2025-03-25 23:02:13 +0100

    feat(dst): accept -seed from command line
    to have deterministic outputs

--- cmd/rnd/main.go
+++ cmd/rnd/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"math/rand/v2"
 
+	"github.com/meshenka/nimble/internal"
 	"github.com/meshenka/nimble/internal/hero"
 )
 
 func main() {
+	// accept a seed for Deterministic setting
+	var dst uint64
+	flag.Uint64Var(&dst, "seed", rand.Uint64(), "set a seed to have deterministic output")
+	flag.Parse()
+	internal.Configure(dst)
 	h := hero.NewHero()
 	fmt.Print(hero.String(h))
 }
