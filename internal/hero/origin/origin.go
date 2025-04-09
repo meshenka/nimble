301701775cd780549be4aeec45a774901b14fbec
meshenka <meshee.knight@gmail.com>
2025-04-09 22:00:43 +0200

    feat(origin): drop extra origins more generic
    outside of the Valley of Hope

--- internal/hero/origin/origin.go
+++ internal/hero/origin/origin.go
@@ -17,6 +17,28 @@ var origins = []string{
 	"The Grawling marches",
 	"The Withering Dunes",
 	"Frostforge mountains",
+
+	// some exta for good measure
+	"a Ragged Outpost",
+	"an Isolated Hamlet",
+	"a Bustling Port",
+	"a Remote Village",
+	"a Trading Hub",
+	"a Sacred Site",
+	"a Forgotten Ruin",
+	"a Nomadic Camp",
+	"a Frozen Wastes",
+	"The Mountain Hall",
+	"a Coastal Settlement",
+	"a Forest Refuge",
+	"a Borderland Fort",
+	"a Fallen Stronghold",
+	"a River Town",
+	"a Hidden Sanctum",
+	"an Ancient Grove",
+	"a Cursed Lands",
+	"a Windswept Cliff",
+	"a Sunken Hold",
 }
 
 func Select() string {
