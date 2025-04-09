13bb2872f7ed732259c66b02326c8010d02c7ff4
meshenka <meshee.knight@gmail.com>
2025-04-09 21:22:13 +0200

    refactor(ancestry): rename all race to ancestry
    to keep consistent with the game language

--- internal/hero/hero.go
+++ internal/hero/hero.go
@@ -3,16 +3,16 @@ package hero
 import (
 	"fmt"
 
+	"github.com/meshenka/nimble/internal/hero/ancestry"
 	"github.com/meshenka/nimble/internal/hero/background"
 	"github.com/meshenka/nimble/internal/hero/class"
 	"github.com/meshenka/nimble/internal/hero/motivation"
 	"github.com/meshenka/nimble/internal/hero/origin"
 	"github.com/meshenka/nimble/internal/hero/quirk"
-	"github.com/meshenka/nimble/internal/hero/race"
 )
 
 type Hero struct {
-	Race       race.Race             `json:"race"`
+	Ancestry   ancestry.Ancestry     `json:"ancestry"`
 	Class      class.Class           `json:"class"`
 	Motivation string                `json:"motivation"`
 	Origin     string                `json:"origin"`
@@ -22,7 +22,7 @@ type Hero struct {
 
 func New() Hero {
 	return Hero{
-		Race:       race.Select(),
+		Ancestry:   ancestry.Select(),
 		Class:      class.Select(),
 		Motivation: motivation.Select(),
 		Background: background.Select(),
@@ -35,7 +35,7 @@ func String(h Hero) string {
 	return fmt.Sprintf("I am a %s %s %s from %s who was %s that end up adventuring cause %s",
 		h.Quirks[0],
 		h.Class.Name,
-		h.Race.Name,
+		h.Ancestry.Name,
 		h.Origin,
 		h.Background.Name,
 		h.Motivation,
